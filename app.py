6520062d2db0c96100c46118bef68f7e3834ccd1
Revati Natu <revatinatu@hotmail.com>
2025-03-23 23:29:06 +0530

    Base UI

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Research Paper Reviewer", page_icon=":memo:")
+
+    st.header("Research Paper Chat :memo:")
+    st.text_input("Ask a question about your papers: ")
+
+    with st.sidebar: # DO NOT ADD A PARENTHESIS HERE
+        st.subheader("Papers: ")
+        st.file_uploader("Upload the pdfs here and click 'Process'")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
