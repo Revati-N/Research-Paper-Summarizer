fa0d2fe84fcce890d63d522c79775ef8da5c5886
Revati Natu <revatinatu@hotmail.com>
2025-03-24 14:43:12 +0530

    api key loading

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv() 
     st.set_page_config(page_title="Research Paper Reviewer", page_icon=":memo:")
 
     st.header("Research Paper Chat :memo:")
