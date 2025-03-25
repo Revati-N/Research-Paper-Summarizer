95472efffc907b1d07d17c7ceaa2b07ad267d7ab
Revati Natu <revatinatu@hotmail.com>
2025-03-25 23:34:14 +0530

    Conversation chain

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS # Runs locally
+from langchain.llms import ollama
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import conversational_retrieval
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -28,17 +31,29 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts = text_chunks, embedding= embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = ollama()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = conversational_retrieval.from_llm(
+        llm = llm,
+        retriever = vectorstore.as_retriever(),
+        memory = memory
+    )
+    return conversation_chain
 
 def main():
     load_dotenv() 
     st.set_page_config(page_title="Research Paper Reviewer", page_icon=":memo:")
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("Research Paper Chat :memo:")
     st.text_input("Ask a question about your papers: ")
 
     with st.sidebar: # DO NOT ADD A PARENTHESIS HERE
         st.subheader("Papers: ")
-        pdf_docs = st.file_uploader("Upload the pdfs here and click 'Process'", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload the papers and click 'Process'", accept_multiple_files=True)
         
         if st.button("Process"):
             with st.spinner("Processing"):
@@ -50,6 +65,9 @@ def main():
 
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
+
+                # Conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == '__main__':
